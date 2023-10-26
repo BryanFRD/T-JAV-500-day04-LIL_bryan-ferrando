c9c2f0298070b13562b20b851d5590841b7de89f
Bryan <bryan.ferrando@epitech.eu>
2023-10-26 08:51:10 +0100

    Fixes

--- Shark.java
+++ Shark.java
@@ -1,8 +1,23 @@
 public class Shark extends Animal {
 
+    private boolean frenzy = false;
+
     public Shark(String name){
         super(name, 0, Type.FISH);
+
         System.out.println("A KILLER IS BORN!");
+    }
+
+    public void smellBlood(boolean blood){
+        this.frenzy = blood;
+    }
+
+    public void status(){
+        if(frenzy){
+            System.out.println(getName() + " is smelling blood and wants to kill.");
+        } else {
+            System.out.println(getName() + " is swimming peacefully.");
+        }
     }
 
 }

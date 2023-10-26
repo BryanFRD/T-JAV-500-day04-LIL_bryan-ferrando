6ec9859c225f0cdd3ce67b716ae0f987f153ba5b
Bryan <bryan.ferrando@epitech.eu>
2023-10-26 11:40:25 +0100

    Fixes

--- GreatShark.java
+++ GreatShark.java
@@ -13,11 +13,11 @@ public class GreatShark extends Shark {
     public void eat(Animal animal) {
         if(animal instanceof Canary){
             System.out.println(getName() + ": Next time you try to give me that to eat, I'll eat you instead.");
-        } else if(animal instanceof Shark){
+        } else if(animal instanceof Shark && super.canEat(animal)){
             System.out.println(getName() + ": The best meal one could wish for.");
+        } else {
+            super.eat(animal);
         }
-
-        super.eat(animal);
     }
 
 }
